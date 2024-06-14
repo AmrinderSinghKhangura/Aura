96fbe190e17476a2cb75f0840805f4cb808d0e51
AmrinderSinghKhangura <amrinder.khangura@edu.sait.ca>
2024-06-13 21:16:50 -0400

    Setup index page and profile

--- app/index.jsx
+++ app/index.jsx
@@ -1,20 +1,15 @@
+import { Link } from "expo-router";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { Text, View } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Aura!</Text>
+    <View className="flex-1 items-center justify-center bg-green-200">
+      <Text className="text-3xl">Aura!</Text>
       <StatusBar style="auto" />
+      <Link href="/profile" style={{ color: "blue" }}>
+        Go to profile
+      </Link>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
